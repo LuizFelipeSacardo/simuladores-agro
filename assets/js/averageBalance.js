6a9f61f3f9bf89288255657a18398f0abc30d44d
Luiz Felipe Sacardo <luizfelipe_1992@hotmail.com>
2022-06-11 14:23:01 -0300

    FirstCommit

---
+++ assets/js/averageBalance.js
@@ -0,0 +1,59 @@
+let averageBalanceSimButton = document.querySelector('#averageBalanceSimButton');
+
+averageBalanceSimButton.addEventListener('click', generateResults);
+
+function daysInEachMonth(){
+  let datePicker = document.querySelector('#datePicker').value;
+  let date = new Date(datePicker);
+  let day = date.getDate()+1;
+  let currentMonthDays = 30 - day;
+  let nextMonthDays = 30 - currentMonthDays;
+
+  let days = [currentMonthDays, nextMonthDays];
+  return days;
+}
+
+function averageBalanceEachMonth(){
+  let days = daysInEachMonth();
+  let averageBalanceInput = document.querySelector('#averageBalance').value;
+  let averageBalance = averageBalanceInput.replace(',', '.');
+
+  let currentMonthValue = days[0] / 30 * averageBalance;
+  let nextMonthValue = days[1] / 30 * averageBalance;
+
+  let values = [currentMonthValue, nextMonthValue];
+
+  console.log(values);
+  return values;
+}
+
+
+function generateResults(){
+  let results = document.querySelector('.results');
+  event.preventDefault();
+  results.classList.remove('hide');
+  averageBalanceSimButton.classList.add('hide');
+
+  let values = averageBalanceEachMonth();  
+  
+  results.innerHTML = `O saldo médio no <strong>Mês Atual</strong> será de <span class="results__output--highlight-main">${values[0].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span> <br><br> O que representa um ganho ou uma perda de <span class="results__output--highlight">${((values[0]+values[1])/30).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span> por dia até o final do mês atual. <br> <br> O saldo médio para o <strong>Próximo Mês</strong> será de <span class="results__output--highlight-main"> ${values[1].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}.</span> <br> <br><button type="submit" class="form__button" id="redoButton">Refazer</button>`
+  
+  let redoButton = document.querySelector('#redoButton');
+  redoButton.addEventListener('click', redoSimulation); 
+  
+  window.scrollBy({
+    top: 2000,
+    behavior: 'smooth'
+  })
+}
+
+function redoSimulation(){
+  let results = document.querySelector('.results');
+  averageBalanceSimButton.classList.remove('hide');
+  results.classList.add('hide');
+
+  window.scrollBy({
+    top: -100,
+    behavior: 'smooth'
+  });
+}
