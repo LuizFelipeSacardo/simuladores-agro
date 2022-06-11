cfbe35ffdc0be29c2ee1504db86e5ed81a7cd3e1
Luiz Felipe Sacardo <luizfelipe_1992@hotmail.com>
2022-06-11 14:37:10 -0300

    Second Commit

--- assets/js/averageBalance.js
+++ assets/js/averageBalance.js
@@ -1,5 +1,4 @@
 let averageBalanceSimButton = document.querySelector('#averageBalanceSimButton');
-
 averageBalanceSimButton.addEventListener('click', generateResults);
 
 function daysInEachMonth(){
@@ -34,17 +33,42 @@ function generateResults(){
   results.classList.remove('hide');
   averageBalanceSimButton.classList.add('hide');
 
+  let errorsList = [];
+
   let values = averageBalanceEachMonth();  
+  errorsList = formValidation();
   
-  results.innerHTML = `O saldo médio no <strong>Mês Atual</strong> será de <span class="results__output--highlight-main">${values[0].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span> <br><br> O que representa um ganho ou uma perda de <span class="results__output--highlight">${((values[0]+values[1])/30).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span> por dia até o final do mês atual. <br> <br> O saldo médio para o <strong>Próximo Mês</strong> será de <span class="results__output--highlight-main"> ${values[1].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}.</span> <br> <br><button type="submit" class="form__button" id="redoButton">Refazer</button>`
   
-  let redoButton = document.querySelector('#redoButton');
-  redoButton.addEventListener('click', redoSimulation); 
-  
-  window.scrollBy({
-    top: 2000,
-    behavior: 'smooth'
-  })
+  if(errorsList.length<=0){
+    results.innerHTML = `O saldo médio no <strong>Mês Atual</strong> será de <span class="results__output--highlight-main">${values[0].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span> <br><br> O que representa um ganho ou uma perda de <span class="results__output--highlight">${((values[0]+values[1])/30).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span> por dia até o final do mês atual. <br> <br> O saldo médio para o <strong>Próximo Mês</strong> será de <span class="results__output--highlight-main"> ${values[1].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}.</span> <br> <br><button type="submit" class="form__button" id="redoButton">Refazer</button>`
+    
+    let redoButton = document.querySelector('#redoButton');
+    redoButton.addEventListener('click', redoSimulation); 
+    
+    window.scrollBy({
+      top: 2000,
+      behavior: 'smooth'
+    })
+  } else{
+    let outputString = `<span class="results__error-title">Atenção, existem erros de preenchimento:</span> <br> <br>`;
+
+    errorsList.forEach(element => {
+      let newElement =  `<span class="results__error-simbol">&#10008</span> <span class="results__error-element"> ${element}</span> <br>`;
+      outputString+=newElement;      
+    });
+
+    let outputHtml = outputString+`<button type="submit" class="form__button" id="redoButton">Refazer</button>`;
+
+    results.innerHTML = outputHtml;
+
+    let redoButton = document.querySelector('#redoButton');
+    redoButton.addEventListener('click', redoSimulation);
+
+    window.scrollBy({
+      top: 2000,
+      behavior: 'smooth'
+    });
+  }
 }
 
 function redoSimulation(){
@@ -56,4 +80,23 @@ function redoSimulation(){
     top: -100,
     behavior: 'smooth'
   });
+}
+
+
+function formValidation(){
+  let errors = [];
+  let datePicker = document.querySelector('#datePicker').value;
+  let averageBalanceInput = document.querySelector('#averageBalance').value;
+
+  if(datePicker.length < 1){
+    let errorDescription = "Selecione uma DATA válida";
+    errors.push(errorDescription);
+  }
+
+  if(averageBalanceInput.length < 1){
+    let errorDescription = "Digite um VALOR válido.";
+    errors.push(errorDescription);
+  }
+
+  return errors;
 }
